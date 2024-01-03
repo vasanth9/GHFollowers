774228fa2064c357746e0e3e0e9401621095265b
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-01-03 20:52:28 +0530

    download and cache image

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by vasanth kumar cheepurupalli on 02/01/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared   = NetworkManager()
-    let baseUrl         = "https://api.github.com/users/"
+    private let baseUrl         = "https://api.github.com/users/"
+    let cache           = NSCache<NSString,UIImage>()
     
     private init() {
         
